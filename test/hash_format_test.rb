1e2ba72190f899b32557552877d68f2b36a5fda2
Josh Buker <crypto@joshbuker.com>
2021-04-27 06:21:33 -0700

    Simplify HashFormat test setup

--- test/hash_format_test.rb
+++ test/hash_format_test.rb
@@ -3,20 +3,15 @@
 require 'test_helper'
 
 class Argon2HashFormatTest < Minitest::Test
+  PASSWORD = 'My secret password'
   OPTIONS = {
-    password: 'My secret password',
     t_cost: 4,
     m_cost: 12,
     p_cost: 3
   }.freeze
 
-  TEMP = Argon2::Password.new(
-    t_cost: OPTIONS[:t_cost],
-    m_cost: OPTIONS[:m_cost],
-    p_cost: OPTIONS[:p_cost]
-  )
-
-  DIGEST = TEMP.create(OPTIONS[:password])
+  TEMP = Argon2::Password.new(OPTIONS)
+  DIGEST = TEMP.create(PASSWORD)
 
   def test_hash_format_variant
     assert argon2 = Argon2::HashFormat.new(DIGEST)
