27793abfbf8b4d0b35556ccce2d895ea6ee55637
Technion <technion@lolware.net>
2022-12-27 14:31:47 +1100

    Bump version. New horrible solution to #56

--- ext/argon2_wrap/extconf.rb
+++ ext/argon2_wrap/extconf.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 require 'mkmf'
-create_makefile('libargon2_wrap')
-File.unlink('Makefile')
 File.rename('Makefile.real', 'Makefile')
+system "make"
--- lib/argon2/version.rb
+++ lib/argon2/version.rb
@@ -3,5 +3,5 @@
 # Standard Gem version constant.
 
 module Argon2
-  VERSION = "2.1.3"
+  VERSION = "2.2.0"
 end
